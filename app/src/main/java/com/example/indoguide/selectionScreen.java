887bc8153ea77d7071bb6f6c8572773f25d94796
saumyaprakash30 <saumyaprakash30@gmail.com>
2020-02-28 17:24:53 +0530

    button error

--- app/src/main/java/com/example/indoguide/selectionScreen.java
+++ app/src/main/java/com/example/indoguide/selectionScreen.java
@@ -26,6 +26,7 @@ public class selectionScreen extends AppCompatActivity {
     private ListView list;
     String data="",startpoint ="";
     String[] startpts = null;
+    int selectedStartPoint=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,35 +37,43 @@ public class selectionScreen extends AppCompatActivity {
         btnStart = findViewById(R.id.btnStartHomeScreen);
         spinner  = findViewById(R.id.spinnerStartingPlace);
         list = findViewById(R.id.lvHints);
+        //hidden on start
+        btnStart.setVisibility(View.GONE);
+        spinner.setVisibility(View.GONE);
 
 
         final ArrayList<String> places = new places().getplaces();
         final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,places);
         list.setAdapter(adapter);
 
-           sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-                @Override
-                public boolean onQueryTextSubmit(String query) {
-                    if(places.contains(query)){
-                        adapter.getFilter().filter(query);
-                    }
-                    return false;
+        sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+//                Toast.makeText(selectionScreen.this, "Sub", Toast.LENGTH_SHORT).show();
+                if(places.contains(query)){
+                    adapter.getFilter().filter(query);
                 }
+//                selectedStartPoint=0;
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+//                if(sv.getVisibility()!=View.GONE)
+//                    selectedStartPoint=0;
+//                Toast.makeText(selectionScreen.this, "change", Toast.LENGTH_SHORT).show();
+                list.setVisibility(View.VISIBLE);
 
-                @Override
-                public boolean onQueryTextChange(String newText) {
-                    adapter.getFilter().filter(newText);
-                    list.setVisibility(View.VISIBLE);
-
-                    return false;
-                }
-            });
+                return false;
+            }
+        });
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
+                selectedStartPoint =1;
                 data = adapter.getItem(i);
                 sv.setQuery(data,false);
+                sv.clearFocus();
                 list.setVisibility(View.GONE);
 
 
@@ -73,25 +82,34 @@ public class selectionScreen extends AppCompatActivity {
         btnPlace.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(data=="EHIPASSIKO"){
+                if(selectedStartPoint==1){
 
                     String[] startpt = {"Select Starting point","1","2","3"};
-                    startpts = startpt;
+                    startpts = startpt; //global variable
                     ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(selectionScreen.this,android.R.layout.simple_spinner_dropdown_item,startpts);
                     spinner.setAdapter(adapter1);
+                    btnStart.setVisibility(View.VISIBLE);
+                    spinner.setVisibility(View.VISIBLE);
 
-
+                }
+                else{
+                    Toast.makeText(selectionScreen.this, "Select Valid Place!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharedPreferences.Editor editor = getSharedPreferences(PLACE_SELECTED,MODE_PRIVATE).edit();
-                editor.putString("place",data);
-                editor.putString("startpoint",startpts[spinner.getSelectedItemPosition()]);
-                editor.commit();
-                selectionScreen.this.finish();
+                if(spinner.getSelectedItemPosition()!=0){
+                    SharedPreferences.Editor editor = getSharedPreferences(PLACE_SELECTED,MODE_PRIVATE).edit();
+                    editor.putString("place",data);
+                    editor.putString("startpoint",startpts[spinner.getSelectedItemPosition()]);
+                    editor.commit();
+                    selectionScreen.this.finish();
+                }
+                else{
+                    Toast.makeText(selectionScreen.this, "Please Select Starting point!", Toast.LENGTH_SHORT).show();
+                }
 ;            }
         });
 
